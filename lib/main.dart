6415995294f28702d13730f5b3c2f36c5e750a77
rghvm <rghvmunjal@gmail.com>
2021-06-13 09:55:47 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
@@ -5,12 +6,44 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+
+  playSound(int num){
+    final player=AudioPlayer();
+    player.setAsset('assets/assets_note$num.wav');
+    player.play();
+  }
+
+  Expanded soundKey(int audioNumber,Color color){
+    return Expanded(
+      child: TextButton(
+        onPressed: (){
+          playSound(audioNumber);
+        },
+        child: Text(''),
+        style:ButtonStyle(backgroundColor:  MaterialStateProperty.all<Color>(color),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              soundKey(1,Colors.purple),
+              soundKey(2,Colors.indigo),
+              soundKey(3,Colors.green),
+              soundKey(4,Colors.orange),
+              soundKey(5,Colors.yellow),
+              soundKey(6,Colors.red),
+              soundKey(7,Colors.teal),
+            ],
+          ),
         ),
       ),
     );
